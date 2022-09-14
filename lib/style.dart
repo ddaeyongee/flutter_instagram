8df8e232243bf725c51cdaa2268c6026d412f4db
taeyongkang <ddaeyongee@gmail.com>
2022-09-14 12:55:44 +0900

    flutter firebase 연동

--- lib/style.dart
+++ lib/style.dart
@@ -1,38 +1,28 @@
 import 'package:flutter/material.dart';
 
 var theme = ThemeData(
-  bottomNavigationBarTheme: BottomNavigationBarThemeData(
-    selectedItemColor: Colors.black,
-  ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: Colors.black,
+    ),
 
-  textButtonTheme: TextButtonThemeData(
-
-  var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));  //GET요청
-var result2 = jsonDecode(result.body);
-// print(result2);
-setState(() {
-data = result2;
-});
-}
-
-@override
-void initState() {
-  super.initState(); style: TextButton.styleFrom(
-    backgroundColor: Colors.grey,
-  )),
+    textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          backgroundColor: Colors.grey,
+        )
+),
 
   elevatedButtonTheme: ElevatedButtonThemeData(
-      style: TextButton.styleFrom(
-    backgroundColor: Colors.grey,
+  style: TextButton.styleFrom(
+  backgroundColor: Colors.grey,
   )),
 
   appBarTheme: AppBarTheme(
-      color: Colors.white,
-      elevation: 1, //그림자크기
-      titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
-      actionsIconTheme: IconThemeData(color: Colors.black,
-      )),
+  color: Colors.white,
+  elevation: 1, //그림자크기
+  titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
+  actionsIconTheme: IconThemeData(color: Colors.black,
+  )),
   textTheme: TextTheme(
-    bodyText2: TextStyle(color: Colors.black),
+  bodyText2: TextStyle(color: Colors.black),
   ),
-);
+  );
